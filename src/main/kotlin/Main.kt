bdf45861c763fde9a38c2c421e78a4c7736bb885
Rupin Raveendra Nath <rupinr@live.com>
2020-01-21 11:52:28 +0100

    Cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,59 +10,70 @@ import java.util.*
 
 
 private const val CLIENT_NAME = "FareScraperApk"
-private const val TOKEN = "TOKEN"
-private const val LOCAL_PATH = "LOCAL_PATH"
-private const val DROPBOX_PATH = "DROPBOX_PATH"
+private const val TOKEN_LONG = "token"
+private const val LOCAL_PATH_LONG = "localpath"
+private const val DROPBOX_PATH_LONG = "dropboxpath"
+private const val FEATURE_LONG = "feature"
+
+private const val TOKEN_SHORT = "t"
+private const val LOCAL_PATH_SHORT = "l"
+private const val DROPBOX_PATH_SHORT = "d"
+private const val FEATURE_SHORT = "f"
 
 private const val NOT_EMPTY_MESSAGE = " cannot be empty"
-private const val FEATURE = "FEATURE"
-private const val UPLOAD = "u"
-private const val DOWNLOAD = "d"
-private const val BACKUP = "b"
+
+
+private const val FEATURE_UPLOAD = "u"
+private const val FEATURE_DOWNLOAD = "d"
+private const val FEATURE_BACKUP = "b"
 
 fun main(args: Array<String>) {
 
+    val cmd = createCmdParser(args)
+    val token = Objects.requireNonNull(cmd.getOptionValue(TOKEN_SHORT), TOKEN_LONG + NOT_EMPTY_MESSAGE)
+    val feature = Objects.requireNonNull(cmd.getOptionValue(FEATURE_SHORT), FEATURE_LONG + NOT_EMPTY_MESSAGE)
+    val dropboxPath = Objects.requireNonNull(cmd.getOptionValue(DROPBOX_PATH_SHORT), DROPBOX_PATH_LONG + NOT_EMPTY_MESSAGE)
+    val localPath = cmd.getOptionValue(LOCAL_PATH_SHORT)
 
-    val options = Options()
-    options.addOption(Option("t", "token", true, "Dropbox Token"))
-    options.addOption(Option("l", "localpath", true, "Local Path"))
-    options.addOption(Option("d", "dropboxpath", true, "Dropbox Path"))
-    options.addOption(Option("f", "feature", true, "Upload or Download"))
-    val parser: CommandLineParser = DefaultParser()
-    val cmd: CommandLine = parser.parse(options, args)
-
-
-    val token = Objects.requireNonNull(cmd.getOptionValue("t"), TOKEN + NOT_EMPTY_MESSAGE)
-    val localPath = Objects.requireNonNull(cmd.getOptionValue("l"), LOCAL_PATH + NOT_EMPTY_MESSAGE)
-    val dropboxPath = Objects.requireNonNull(cmd.getOptionValue("d"), DROPBOX_PATH + NOT_EMPTY_MESSAGE)
-    val feature = Objects.requireNonNull(cmd.getOptionValue("f"), FEATURE + NOT_EMPTY_MESSAGE)
+    if(feature!= FEATURE_BACKUP){
+            Objects.requireNonNull(localPath, LOCAL_PATH_LONG + NOT_EMPTY_MESSAGE)
+    }
 
     val config = DbxRequestConfig.newBuilder(CLIENT_NAME).build()
     val client = DbxClientV2(config, token)
     when (feature) {
-        UPLOAD -> uploadFile(client, localPath, dropboxPath)
-        DOWNLOAD -> downloadFile(client, localPath, dropboxPath)
-        BACKUP -> backupFile(client, dropboxPath)
+        FEATURE_UPLOAD -> uploadFile(client, localPath, dropboxPath)
+        FEATURE_DOWNLOAD -> downloadFile(client, localPath, dropboxPath)
+        FEATURE_BACKUP -> backupFile(client, dropboxPath)
         else -> print("Unknown feature")
     }
 }
 
 
-fun backupFile(clientV2: DbxClientV2, dropboxPath: String){
+fun createCmdParser(args: Array<String>): CommandLine {
+    val options = Options()
+    options.addOption(Option(TOKEN_SHORT, TOKEN_LONG, true, "Dropbox Token"))
+    options.addOption(Option(LOCAL_PATH_SHORT, LOCAL_PATH_LONG, true, "Local Path"))
+    options.addOption(Option(DROPBOX_PATH_SHORT, DROPBOX_PATH_LONG, true, "Dropbox Path"))
+    options.addOption(Option(FEATURE_SHORT, FEATURE_LONG, true, "Upload or Download"))
+    val parser: CommandLineParser = DefaultParser()
+   return parser.parse(options, args)
+}
 
-    val metaData= clientV2.files().moveV2(dropboxPath,dropboxPath+System.currentTimeMillis()).metadata
-    print(dropboxPath+ "moved to "+metaData.pathDisplay)
+fun backupFile(clientV2: DbxClientV2, dropboxPath: String){
+    val metaData= clientV2.files().moveV2(dropboxPath,dropboxPath+"_BACKUP_"+System.currentTimeMillis()).metadata
+    println(dropboxPath+ "moved to "+metaData.pathDisplay)
 }
 
 fun uploadFile(clientV2: DbxClientV2, localPath: String, dropBoxPath: String) {
     FileInputStream(localPath).use {
         val metadata = clientV2.files().uploadBuilder(dropBoxPath).uploadAndFinish(it)
-        print(metadata.name + " Uploaded on " + metadata.clientModified)
+        println(metadata.name + " Uploaded on " + metadata.clientModified)
     }
 }
 
 fun downloadFile(clientV2: DbxClientV2, localPath: String, dropboxPath: String) {
     val file = File(localPath)
     val metaData = FileOutputStream(file).use { clientV2.files().download(dropboxPath).download(it) }
-    print("Downloaded file to " + localPath + " at " + metaData.clientModified)
+    println("Downloaded file to " + localPath + " at " + metaData.clientModified)
 }
